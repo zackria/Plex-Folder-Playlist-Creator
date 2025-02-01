8952b2a3aeddc9edb1129c8de29cd50888224b20
Zack Dawood <zackria@gmail.com>
2025-02-01 17:10:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -201,7 +201,7 @@ function createWindow() {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
       nodeIntegration: false,
-      devTools: true,
+      devTools: false,
     },
   });
 }
