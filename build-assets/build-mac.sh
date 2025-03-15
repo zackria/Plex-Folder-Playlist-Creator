203e4f4cddf914132f36f65eda5a95c39f41d217
Zack Dawood <zackria@gmail.com>
2025-03-15 15:10:32 -0400

    Update build-mac.sh

--- build-assets/build-mac.sh
+++ build-assets/build-mac.sh
@@ -1,3 +1,6 @@
+export APPLE_ID="username@gmail.com"
+export APPLE_APP_SPECIFIC_PASSWORD="MY_APP_SPECIFIC_PASSWORD"
+export APPLE_TEAM_ID="MY_TEAM_ID"
 export CSC_LINK-=Cetrifcate.p12
 export CSC_KEY_PASSWORD=password
 
